dab09ab37cdf98efd5890ac64836d70ca7bf46be
Maria Laura Tarabillo <mlta18@outlook.com>
2017-07-31 19:28:19 -0400

    Commented all my session code its not working

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser")
 // This are outside so any function can accese the data.
 const todoList = []
 const completedList = []
-const expressSession = require("express-session")
+//const expressSession = require("express-session")
 const app = express()
 
 app.engine("mustache", mustacheExpress())
@@ -17,13 +17,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(expressValidator())
 
-app.use(
-  expressSession({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true
-  })
-)
+// app.use(
+//   expressSession({
+//     secret: "keyboard cat",
+//     resave: false,
+//     saveUninitialized: true
+//   })
+// )
 
 // Create the home for my form
 app.get("/", (request, response) => {
@@ -35,11 +35,13 @@ app.get("/", (request, response) => {
 })
 // when I'm creating the todo's
 // i'm using post because I'm giving information to the server, because I have created data.
+
 app.post("/addTodo", (request, response) => {
   const descriptionForNewTodo = request.body.description
   todoList.push(descriptionForNewTodo)
   response.redirect("/")
 })
+
 // When I have mark Complete goes to the complete section
 app.post("/markComplete", (request, response) => {
   const descriptionOfTheTaskWeAreCompleting = request.body.descriptionOfTheTaskWeAreCompleting
@@ -47,24 +49,24 @@ app.post("/markComplete", (request, response) => {
 
   const indexOfItem = todoList.indexOf(descriptionOfTheTaskWeAreCompleting)
   todoList.splice(indexOfItem, 1)
-
-  // Session
-  const todoList = request.session.todoList || []
-
-  const id = parseInt(request.body.id)
-
-  const todo = todoList.find(todo => todo.id === id)
-
-  if (todo) {
-    todo.completed = true
-
-    // Place the todolist back in the session
-    request.session.todoList = todoList
-  }
+  //
+  // // Session
+  // const todoList = request.session.todoList || []
+  //
+  // const id = parseInt(request.body.id)
+  //
+  // const todo = todoList.find(todo => todo.id === id)
+  //
+  // if (todo) {
+  //   todo.completed = true
+  //
+  //   // Place the todolist back in the session
+  //   request.session.todoList = todoList
+  // }
 
   response.redirect("/")
 })
 
 app.listen(3000, () => {
-  console.log("Funiona")
+  console.log("Funciona")
 })
